710c09f5a46d67d42e2edc2948cdf7b591d35bfb
austin <amitchell80@gatech.edu>
2022-08-02 13:03:19 -0400

    Added necessary additions to deploy site.

--- index.js
+++ index.js
@@ -14,13 +14,12 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use('/contact-list', contactRoutes);
 
-app.get('/', (req, res => {
-    res.setEncoding('Hello to Memories API');
-}));
+app.get('/', (req, res) => {
+    res.send('Hello to Memories API');
+});
 
-const PORT = process.env.PORT;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => app.listen(PORT, () => console.log(`Server Running on port:  ${PORT}`)))
+    .then(() => app.listen(process.env.PORT || 5000, '0.0.0.0', () => console.log(`Server Running on port:  ${process.env.PORT || 5000}`)))
     .catch((error) => console.log(error));
 // https://mongodb.com/cloud/atlas
