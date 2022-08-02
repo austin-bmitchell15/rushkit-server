8b803d2f8799c3d9adf26afebe0b1b7d383fb340
austin <amitchell80@gatech.edu>
2022-08-02 11:31:47 -0400

    Created environment variables for the backend.

--- index.js
+++ index.js
@@ -2,20 +2,21 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv'
 
 import contactRoutes from './routes/contactRoutes.js'
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use('/contact-list', contactRoutes);
 
-const CONNECTION_URL = "mongodb+srv://technologychair:Carnation@cluster0.qlkh9.mongodb.net/?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server Running on port:  ${PORT}`)))
     .catch((error) => console.log(error));
 // https://mongodb.com/cloud/atlas
