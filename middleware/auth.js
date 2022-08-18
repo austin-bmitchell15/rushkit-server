2f71b274cb2a68fa5ddef11404c52914756930c7
austin-bmitchell15 <austin.bmitchell15@gmail.com>
2022-08-18 17:26:23 -0400

    Got rid of random console logs.

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,6 @@ dotenv.config();
 const auth = async (req, res, next) => {
     try {
         const token = req.headers['authorization'].split(" ")[1];
-        console.log(token);
 
         let decodedData;
 
